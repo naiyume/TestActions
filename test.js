909b4097e64dbc178c3272cd3c49b77ab557a349
naiyume <naiyume>
2020-05-17 17:59:38 -0400

    changed some things for testing

--- test.js
+++ test.js
@@ -1,12 +1,1 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-  
-  let final = ''
-  readline.question(``, (name) => {
-    final += name;
-    readline.close()
-  })
-
-  console.log(final)
+console.log('testing abc')
