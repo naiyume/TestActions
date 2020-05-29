284820b40a5c3229ad727d8405043d7202e7cd01
naiyume <naiyume>
2020-05-29 11:08:05 -0400

    trying to just pipe it

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const concept = require('./concept');
 const fs = require('fs');
 const otherDirection = require('./otherDirection');
-let files = fs.readFileSync('$HOME/files.csv')
+let files = fs.readFileSync('files.csv')
 for(let i = 0; i < files.length; i++){
     let file = files[i]
     if(file.includes('.csv')){
